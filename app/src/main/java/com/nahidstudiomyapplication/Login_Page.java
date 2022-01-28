ec8171d576bacdcb41e885f310e92f6594e619aa
depokdas <arzunchandradas125@gmail.com>
2022-01-27 22:43:07 -0800

    Login and Registration page complete

---
+++ app/src/main/java/com/nahidstudiomyapplication/Login_Page.java
@@ -0,0 +1,32 @@
+package com.nahidstudiomyapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login_Page extends AppCompatActivity {
+
+    Button login_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        login_button = findViewById(R.id.login_button);
+
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login_Page.this, Registration_Page.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/nahidstudiomyapplication/MainActivity.java
+++ app/src/main/java/com/nahidstudiomyapplication/MainActivity.java
@@ -2,11 +2,16 @@ package com.nahidstudiomyapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.nahidstudiomyapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
 
     ActivityMainBinding binding;
@@ -15,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login_Page.class);
+                startActivity(intent);
+            }
+        });
 
 
 
